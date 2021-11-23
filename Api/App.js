16d1d1a79b751b25089074497fc88a95a7b268d3
Juan <jcmm150@hotmail.com>
2021-11-22 22:16:36 -0500

    First commit Camilo

--- Api/App.js
+++ Api/App.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hello World Junior XD");
+  res.send("Hello World Camilo XD");
 });
 
 app.listen(3000);
