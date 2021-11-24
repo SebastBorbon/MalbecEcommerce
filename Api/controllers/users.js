923d361fa2dd5529b7f450613219266dbfb28778
Sebastian <seborbon@outlook.com>
2021-11-24 00:27:28 -0500

    Passwords crypted

--- Api/controllers/users.js
+++ Api/controllers/users.js
@@ -1,4 +1,13 @@
-const registerUser = (username, password) => {};
+const bcrypt = require("bcrypt");
 
+const registerUser = async (username, password) => {
+  let hashedPwd = await bcrypt.hash(password, 10);
+};
 const getUser = (userId) => {};
 const checkUserCredentials = (email, password) => {};
+
+module.exports = {
+  registerUser,
+  getUser,
+  checkUserCredentials,
+};
