d821122ab288e29fbc3657cf33554b6022e76394
Sebastian <seborbon@outlook.com>
2021-11-29 22:12:55 -0500

    create db

--- Api/routers/auth.js
+++ Api/routers/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { registerUser } = require("../controllers/users");
 
 // const UserModel = mongoose.model("UserModel", {
 //   userName: String,
@@ -17,5 +18,19 @@ router.route("/signup").get((req, res) => {
   res.send("hello sign up");
 });
 
-router.route("/signup").post((req, res) => {});
+router.route("/signup").post(async (req, res) => {
+  console.log(req.body);
+  const { userName, password } = req.body;
+
+  try {
+    if (req.body) {
+      await registerUser(userName, password);
+      res.send("user registred");
+    } else {
+      res.status(404).send({ message: "ta mal el user" });
+    }
+  } catch (err) {
+    res.status(404).send({ message: "ta mal" });
+  }
+});
 exports.router = router;
