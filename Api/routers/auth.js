d938cc304b4e3d98b012a329a25681bc9d18a131
Sebastian <seborbon@outlook.com>
2021-11-30 23:39:20 -0500

    add function sign up and generate tokens to mantain session

--- Api/routers/auth.js
+++ Api/routers/auth.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser } = require("../controllers/users");
+const { registerUser, checkUserCredentials } = require("../controllers/users");
+const jwt = require("jsonwebtoken");
+const UserSchema = require("../models/user");
 
 router.route("/signup").get((req, res) => {
   res.send("hello sign up");
@@ -9,7 +11,6 @@ router.route("/signup").get((req, res) => {
 router.route("/signup").post(async (req, res) => {
   console.log(req.body);
   const { userName, password } = req.body;
-
   try {
     if (req.body) {
       await registerUser(userName, password);
@@ -21,4 +22,22 @@ router.route("/signup").post(async (req, res) => {
     res.status(404).send({ message: "ta mal" });
   }
 });
+
+router.route("/login").post(async (req, res) => {
+  const { userName, password } = req.body;
+  try {
+    let user = await checkUserCredentials(userName, password);
+    console.log(user);
+    //if all the user data is correct, we send back the user
+    if (user) {
+      const token = jwt.sign({ userName: user.userName }, "secretPassword");
+      res.status(200).json({ token });
+    } else {
+      res.status(400).send({ message: "incorrect Password" });
+    }
+  } catch (err) {
+    res.status(404).send({ message: "connecting to DB" });
+  }
+});
+
 exports.router = router;
