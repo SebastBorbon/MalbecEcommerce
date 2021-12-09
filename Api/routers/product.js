385e65ff188409e8d4a76d3fc886fb7ed6b40447
Sebastian <seborbon@outlook.com>
2021-12-08 20:24:54 -0500

    product route finished

---
+++ Api/routers/product.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const router = express.Router();
+const {
+  verifyAuthorization,
+  verifyAdmin,
+} = require("../middlewares/verifyToken");
+const Product = require("../models/Product");
+
+router.post("/", verifyAdmin, async (req, res) => {
+  const newProduct = new Product(req.body);
+  try {
+    const savedProduct = await newProduct.save();
+    res.json("product created");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:id", verifyAdmin, async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.json(updatedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE product
+router.delete("/:id", verifyAdmin, async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.json("Product has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET product
+router.get("/find/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Get all products
+router.get("/", verifyAdmin, async (req, res) => {
+  const queryNew = req.query.new;
+  const queryCategory = req.query.category;
+  try {
+    let products;
+    if (queryNew) {
+      products = await Product.find().sort({ createdAt: -1 }).limit(5);
+    } else if (queryCategory) {
+      products = await Product.find({
+        categories: {
+          $in: [queryCategory],
+        },
+      });
+    } else {
+      products = await Product.find();
+    }
+    res.json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+exports.router = router;
