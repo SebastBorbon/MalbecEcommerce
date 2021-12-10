1a4c90dcb2adc1586d3fb84a17ff54e393e9b72d
Sebastian <seborbon@outlook.com>
2021-12-09 22:43:36 -0500

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import LogIn from "./components/LogIn/LogIn";
+import Pay from "./components/pay/Pay";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import React from "react";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<LogIn />} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<LogIn />} />
+        <Route exact path="/payment" element={<Pay />} />
       </Routes>
     </>
   );
---
+++ client/src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://www.43einhalb.com/media/233851/w/2000/h/1200/n/nike-air-jordan-1-mid-gs-pink-quartz-555112-603-mood-3.jpg",
+    title: "Nike Air Jordan 1 Mid GS Pink Quartz",
+    description: "",
+    background: "F5D9F0",
+  },
+  {
+    id: 2,
+    img: "http://www.cordonsnegres.com/wp-content/uploads/air-jordan-1-retro-high-chicago-01.jpg",
+    title: "Nike Air Jordan 1 OG Chicago",
+    description: "EY ESTA SI TIENE DESCRIPCION",
+    background: "46021A",
+  },
+  {
+    id: 3,
+    img: "https://images.wave.fr/images//air-jordan-1-x-j-balvin-release-date-1000x750.jpg",
+    title: "Nike Air Jordan 1 J BALVIN",
+    description: "EL CACHON ESTA PEGAO",
+    background: "46021A",
+  },
+];
