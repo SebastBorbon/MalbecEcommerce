edff6a139dbd25171b224c89f57a85daba45deb2
Sebastian <seborbon@outlook.com>
2021-12-09 22:42:45 -0500

    create Announcement component

---
+++ client/src/components/announcement/Announcement.jsx
@@ -0,0 +1,11 @@
+import "./Announcement.css";
+
+const Announcement = () => {
+  return (
+    <div className="AnnouncementContainer">
+      ONLY FOR HOLIDAYS! Free shipping on orders over 100$
+    </div>
+  );
+};
+
+export default Announcement;
