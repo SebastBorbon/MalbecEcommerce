9594797f127b4989741ebe30fef49b9ffe87d84b
Sebastian <seborbon@outlook.com>
2021-12-11 03:04:04 -0500

    Small changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,16 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import React from "react";
 import Home from "./pages/Home";
+import ProductList from "./pages/producList/ProductList";
+import Product from "./pages/ProductPage/Product";
 
 function App() {
   return (
     <>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/ProductList" element={<ProductList />} />
+        <Route path="/Product" element={<Product />} />
         <Route exact path="/login" element={<LogIn />} />
         <Route exact path="/payment" element={<Pay />} />
       </Routes>
--- client/src/components/announcement/Announcement.jsx
+++ client/src/components/announcement/Announcement.jsx
@@ -3,7 +3,7 @@ import "./Announcement.css";
 const Announcement = () => {
   return (
     <div className="AnnouncementContainer">
-      ONLY FOR HOLIDAYS! Free shipping on orders over 100$
+      ONLY FOR HOLIDAYS! Free shipping in orders over 100$
     </div>
   );
 };
