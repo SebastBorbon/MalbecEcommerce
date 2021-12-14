96c7da34c9c0411dd1f63ff93d78edc4c71a2718
Sebastian <seborbon@outlook.com>
2021-12-13 20:48:29 -0500

    Product page done

--- client/src/pages/ProductPage/Product.jsx
+++ client/src/pages/ProductPage/Product.jsx
@@ -4,8 +4,47 @@ import Navbar from "../../components/navbar/Navbar";
 import "./Product.css";
 import RemoveIcon from "@mui/icons-material/Remove";
 import AddIcon from "@mui/icons-material/Add";
+import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { addProduct } from "../../redux/cartRedux";
+import { useDispatch } from "react-redux";
 
 const Product = () => {
+  const location = useLocation();
+  const id = location.pathname.split("/")[2];
+  const [product, setProduct] = useState({});
+  const [quantity, setQuantity] = useState(1);
+  const [size, setSize] = useState("");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const res = await axios.get(
+          "http://localhost:3000/api/products/find/" + id
+        );
+
+        setProduct(res.data);
+        console.log(res.data);
+      } catch {}
+    };
+    getProduct();
+  }, [id]);
+
+  const handleQuantity = (type) => {
+    if (type === "dec") {
+      quantity > 1 && setQuantity(quantity - 1);
+    } else {
+      setQuantity(quantity + 1);
+    }
+  };
+
+  const handleClick = () => {
+    //update cart
+    dispatch(addProduct({ ...product, quantity, size }));
+  };
+
   return (
     <div className="Product-Container">
       <Navbar />
@@ -14,36 +53,43 @@ const Product = () => {
         <div className="Product-ImgContainer">
           <img
             className="Product-Img"
-            src="https://gamarraecommerce.com/wp-content/uploads/2020/06/Jordan-1-Mid-Milan-2.jpg"
+            src={product.imageUrl}
             alt="can't charge img"
           />
         </div>
         <div className="Product-InfoContainer">
-          <h1 className="Title-InfoProduct">JORDAN 1 MILAN 2</h1>
-          <p className="Desc-InfoProduct">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            venenatis, dolor in finibus malesuada, lectus ipsum porta nunc, at
-            iaculis arcu nisi sed mauris. Nulla fermentum vestibulum ex, eget
-            tristique tortor pretium ut. Curabitur elit justo, consequat id
-            condimentum ac, volutpat ornare
-          </p>
-          <span className="Price-InfoProduct">$ 100</span>
+          <h1 className="Title-InfoProduct">{product.title}</h1>
+          <p className="Desc-InfoProduct">{product.description}</p>
+          <span className="Price-InfoProduct">$ {product.price}</span>
 
           <div className="Product-FilterContainer">
-            <button className="Product-BtnSize">9</button>
-            <button className="Product-BtnSize">10</button>
-            <button className="Product-BtnSize">11</button>
-            <button className="Product-BtnSize">11.5</button>
-            <button className="Product-BtnSize">12</button>
-            <button className="Product-BtnSize">13</button>
+            {product.size?.map((size) => {
+              return (
+                <button
+                  className="Product-BtnSize"
+                  key={size}
+                  onClick={() => setSize(size)}
+                >
+                  {size}
+                </button>
+              );
+            })}
           </div>
           <div className="Product-AddContainer">
             <div className="Product-AmountContainer">
-              <RemoveIcon />
-              <span className="Product-Amount">1</span>
-              <AddIcon />
+              <RemoveIcon
+                style={{ cursor: "pointer" }}
+                onClick={() => handleQuantity("dec")}
+              />
+              <span className="Product-Amount">{quantity}</span>
+              <AddIcon
+                style={{ cursor: "pointer" }}
+                onClick={() => handleQuantity("inc")}
+              />
             </div>
-            <button className="Product-BtnCart">ADD TO CART</button>
+            <button className="Product-BtnCart" onClick={handleClick}>
+              ADD TO CART
+            </button>
           </div>
         </div>
       </div>
