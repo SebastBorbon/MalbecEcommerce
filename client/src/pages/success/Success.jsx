27244121059f451ab1322de2dd94ebcf5325292d
Sebastian <seborbon@outlook.com>
2021-12-13 20:47:39 -0500

    todo Change the userId

---
+++ client/src/pages/success/Success.jsx
@@ -0,0 +1,47 @@
+import "./Success.css";
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Success = () => {
+  const location = useLocation();
+  const data = location.state.data;
+  const cart = location.state.products;
+  const [orderId, setOrderId] = useState(null);
+  console.log(location.state);
+  useEffect(() => {
+    const createOrder = async () => {
+      try {
+        const res = await axios.post("http://localhost:3000/api/orders", {
+          userId:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxYjE0ZmM5MWM3ZWMzNTYxZjRjYjNiMCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzOTQzNjEzMH0.mAWnGaeeppSJOliicW3g4w_LCKjqIL2jfu4xjEe20p8",
+          products: cart.products.map((item) => ({
+            productId: item._id,
+            quantity: item._quantity,
+          })),
+          amount: cart.total,
+          address: data.billing_details.address,
+        });
+        setOrderId(res.data._id);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    if (data) {
+      createOrder();
+    }
+  }, [cart, data]);
+  return (
+    <div className="Success-Container">
+      {orderId
+        ? `Order has been created successfully. Your order number is ${orderId}`
+        : `Successfull. Your order is being prepared...`}
+      <Link to="/">
+        <button className="btnSuccess">Go to Homepage</button>
+      </Link>
+    </div>
+  );
+};
+
+export default Success;
