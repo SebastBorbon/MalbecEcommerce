bcd2515c1f357f1cb04fe9f3e69bc5bd48ad23c1
Sebastian <seborbon@outlook.com>
2021-12-14 20:18:59 -0500

    persist reducer created

---
+++ client/src/redux/apiCalls.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { loginStart, loginFailure, loginSuccess } from "./userReducer";
+
+export const login = async (dispatch, user) => {
+  dispatch(loginStart());
+  try {
+    const res = await axios.post("http://localhost:3000/api/auth/login", user);
+    dispatch(loginSuccess(res.data));
+  } catch (err) {
+    dispatch(loginFailure());
+  }
+};
