f745567a396f6498349745a0b88e102d6768d90a
Sebastian <seborbon@outlook.com>
2021-12-14 20:18:17 -0500

    register finished!

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,21 +1,76 @@
 import "./Register.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { login } from "../../redux/apiCalls";
+import { useDispatch } from "react-redux";
 
 const Register = () => {
+  const [inputs, setInputs] = useState([]);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/api/auth/signup",
+        inputs
+      );
+      if (res.data === "new user created") {
+        login(dispatch, {
+          userName: inputs.userName,
+          password: inputs.password,
+        });
+        navigate("/");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="Register-Container">
       <div className="Register-Wrapper">
         <h1 className="Register-Title">SIGN UP</h1>
         <form className="Register-Form">
-          <input className="Register-Input" placeholder="Name" />
-          <input className="Register-Input" placeholder="Last Name" />
-          <input className="Register-Input" placeholder="Email" />
-          <input className="Register-Input" placeholder="Password" />
-          <input className="Register-Input" placeholder="Confirm Password" />
+          <input
+            name="userName"
+            className="Register-Input"
+            placeholder="userName"
+            onChange={handleChange}
+          />
+          <input
+            name="email"
+            className="Register-Input"
+            placeholder="Email"
+            onChange={handleChange}
+          />
+          <input
+            name="password"
+            type="password"
+            className="Register-Input"
+            placeholder="Password"
+            onChange={handleChange}
+          />
+          <input
+            name="address"
+            className="Register-Input"
+            placeholder="Address"
+            onChange={handleChange}
+          />
           <span className="Register-Agreement">
             By creating an account, I consent to the processing of my personal
             data in accordance with the <b>PRIVACY POLICY</b>
           </span>
-          <button className="BtnRegister">SIGN UP</button>
+          <button className="BtnRegister" onClick={handleClick}>
+            SIGN UP
+          </button>
         </form>
       </div>
     </div>
