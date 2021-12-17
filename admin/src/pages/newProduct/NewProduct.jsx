bb8b788125e4d2f5fa3f4180e7907ade8ad571cf
Sebastian <seborbon@outlook.com>
2021-12-16 20:33:48 -0500

    all product pages finished!

--- admin/src/pages/newProduct/NewProduct.jsx
+++ admin/src/pages/newProduct/NewProduct.jsx
@@ -1,30 +1,104 @@
 import "./newProduct.css";
+import { useState } from "react";
+import { addProducts } from "../../redux/apiCalls";
+import { useDispatch } from "react-redux";
 
 export default function NewProduct() {
+  const [size, setSizes] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [inputs, setInputs] = useState({});
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleSizes = (e) => {
+    setCategories(e.target.value.split(","));
+  };
+  const handleCategories = (e) => {
+    setSizes(e.target.value.split(","));
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault(e);
+    const product = { ...inputs, categories, size };
+    addProducts(product, dispatch);
+  };
+
   return (
     <div className="newProduct">
       <h1 className="addProductTitle">New Product</h1>
       <form className="addProductForm">
         <div className="addProductItem">
           <label>Image</label>
-          <input type="file" id="file" />
+          <input
+            name="imageUrl"
+            type="text"
+            id="file"
+            placeholder="image link"
+            onChange={handleChange}
+          />
         </div>
         <div className="addProductItem">
-          <label>Name</label>
-          <input type="text" placeholder="Apple Airpods" />
+          <label>Product Name</label>
+          <input
+            name="title"
+            type="text"
+            placeholder="Shoes Name"
+            onChange={handleChange}
+          />
         </div>
         <div className="addProductItem">
-          <label>Stock</label>
-          <input type="text" placeholder="123" />
+          <label>description</label>
+          <input
+            name="description"
+            type="text"
+            placeholder="Description..."
+            onChange={handleChange}
+          />
         </div>
         <div className="addProductItem">
-          <label>Active</label>
-          <select name="active" id="active">
-            <option value="yes">Yes</option>
-            <option value="no">No</option>
+          <label>Price</label>
+          <input
+            name="price"
+            type="number"
+            placeholder="$  USD"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="addProductItem">
+          <label>Size</label>
+          <input type="text" placeholder="text" onChange={handleSizes} />
+        </div>
+        <div className="addProductItem">
+          <label>Categories</label>
+          <input type="text" placeholder="text" onChange={handleCategories} />
+        </div>
+        <div className="addProductItem">
+          <label>Brand</label>
+          <select name="brand" onChange={handleChange}>
+            <option value="adidas">adidas</option>
+            <option value="nike">nike</option>
+            <option value="jordan">jordan</option>
+            <option value="new balance">new balance</option>
+            <option value="puma">puma</option>
+            <option value="off white">off white</option>
           </select>
         </div>
-        <button className="addProductButton">Create</button>
+
+        <div className="addProductItem">
+          <label>Stock</label>
+          <select name="Stock" onChange={handleChange}>
+            <option value="true">Yes</option>
+            <option value="false">No</option>
+          </select>
+        </div>
+        <button className="addProductButton" onClick={handleClick}>
+          Create
+        </button>
       </form>
     </div>
   );
