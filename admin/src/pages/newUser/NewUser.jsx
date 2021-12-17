e68d8ee813992c953b29656b6ab72655ec4c5c16
Sebastian <seborbon@outlook.com>
2021-12-17 02:00:49 -0500

    todo solve refresh problem

--- admin/src/pages/newUser/NewUser.jsx
+++ admin/src/pages/newUser/NewUser.jsx
@@ -1,53 +1,67 @@
 import "./newUser.css";
+import { useState } from "react";
+import { addMalbecUsers } from "../../redux/apiCalls";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
 export default function NewUser() {
+  const [inputs, setInputs] = useState({});
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+  const handleClick = (e) => {
+    e.preventDefault(e);
+    const user = { ...inputs };
+    addMalbecUsers(user, dispatch);
+    window.location.reload();
+  };
   return (
     <div className="newUser">
       <h1 className="newUserTitle">New User</h1>
       <form className="newUserForm">
         <div className="newUserItem">
           <label>Username</label>
-          <input type="text" placeholder="john" />
-        </div>
-        <div className="newUserItem">
-          <label>Full Name</label>
-          <input type="text" placeholder="John Smith" />
+          <input
+            onChange={handleChange}
+            type="text"
+            placeholder="username"
+            name="userName"
+          />
         </div>
         <div className="newUserItem">
           <label>Email</label>
-          <input type="email" placeholder="john@gmail.com" />
+          <input
+            onChange={handleChange}
+            type="email"
+            placeholder="email"
+            name="email"
+          />
         </div>
         <div className="newUserItem">
           <label>Password</label>
-          <input type="password" placeholder="password" />
-        </div>
-        <div className="newUserItem">
-          <label>Phone</label>
-          <input type="text" placeholder="+1 123 456 78" />
+          <input
+            onChange={handleChange}
+            type="password"
+            placeholder="password"
+            name="password"
+          />
         </div>
         <div className="newUserItem">
           <label>Address</label>
-          <input type="text" placeholder="New York | USA" />
+          <input
+            onChange={handleChange}
+            type="text"
+            placeholder="Address"
+            name="address"
+          />
         </div>
-        <div className="newUserItem">
-          <label>Gender</label>
-          <div className="newUserGender">
-            <input type="radio" name="gender" id="male" value="male" />
-            <label htmlfor="male">Male</label>
-            <input type="radio" name="gender" id="female" value="female" />
-            <label htmlfor="female">Female</label>
-            <input type="radio" name="gender" id="other" value="other" />
-            <label htmlfor="other">Other</label>
-          </div>
-        </div>
-        <div className="newUserItem">
-          <label>Active</label>
-          <select className="newUserSelect" name="active" id="active">
-            <option value="yes">Yes</option>
-            <option value="no">No</option>
-          </select>
-        </div>
-        <button className="newUserButton">Create</button>
+        <button onClick={handleClick} className="newUserButton">
+          Create
+        </button>
       </form>
     </div>
   );
