822d353c3e42429743306773c4c0ffaf7281834e
Sebastian <seborbon@outlook.com>
2021-12-16 20:34:08 -0500

    Product redux crud created

---
+++ admin/src/redux/productReducer.js
@@ -0,0 +1,86 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const productSlice = createSlice({
+  name: "product",
+  initialState: {
+    products: [],
+    isFetching: false,
+    error: false,
+  },
+  reducers: {
+    //get all
+    getProductStart: (state) => {
+      state.isFetching = true;
+      state.error = false;
+    },
+    getProductSuccess: (state, action) => {
+      state.products = action.payload;
+      state.isFetching = false;
+    },
+    getProductFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    },
+    //delete
+    deleteProductStart: (state) => {
+      state.isFetching = true;
+      state.error = false;
+    },
+    deleteProductSuccess: (state, action) => {
+      state.products.splice(
+        state.products.findIndex((item) => item._id === action.payload),
+        1
+      );
+      state.isFetching = false;
+    },
+    deleteProductFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    },
+    //update
+    updateProductStart: (state) => {
+      state.isFetching = true;
+      state.error = false;
+    },
+    updateProductSuccess: (state, action) => {
+      state.products[
+        state.products.findIndex((item) => item._id === action.payload.id)
+      ] = action.payload.product;
+      state.isFetching = false;
+    },
+    updateProductFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    },
+    //update
+    addProductStart: (state) => {
+      state.isFetching = true;
+      state.error = false;
+    },
+    addProductSuccess: (state, action) => {
+      state.products.push(action.payload);
+      state.isFetching = false;
+    },
+    addProductFailure: (state) => {
+      state.isFetching = false;
+      state.error = true;
+    },
+  },
+});
+
+export const {
+  getProductStart,
+  getProductSuccess,
+  getProductFailure,
+  deleteProductStart,
+  deleteProductSuccess,
+  deleteProductFailure,
+  updateProductStart,
+  updateProductSuccess,
+  updateProductFailure,
+  addProductStart,
+  addProductSuccess,
+  addProductFailure,
+} = productSlice.actions;
+
+export default productSlice.reducer;
