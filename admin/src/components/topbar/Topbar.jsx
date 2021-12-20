8b5e6ba2062fab37ccb48350d071bf3ae02e37f8
Sebastian <seborbon@outlook.com>
2021-12-20 18:29:19 -0500

    admin components finished

--- admin/src/components/topbar/Topbar.jsx
+++ admin/src/components/topbar/Topbar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./topbar.css";
-import { NotificationsNone, Language, Settings } from "@material-ui/icons";
 
 export default function Topbar() {
   return (
@@ -10,19 +9,8 @@ export default function Topbar() {
           <span className="logo">Malbec Admin</span>
         </div>
         <div className="topRight">
-          <div className="topbarIconContainer">
-            <NotificationsNone />
-            <span className="topIconBadge">2</span>
-          </div>
-          <div className="topbarIconContainer">
-            <Language />
-            <span className="topIconBadge">2</span>
-          </div>
-          <div className="topbarIconContainer">
-            <Settings />
-          </div>
           <img
-            src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            src="https://raw.githubusercontent.com/SebastBorbon/Portfolio/main/public/profileellie.png"
             alt=""
             className="topAvatar"
           />
